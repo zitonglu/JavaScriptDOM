e5a57009633a1ed4843eef396b57c6ec9d001579
紫铜炉 <910109610@qq.com>
2016-07-04 14:23:16 +0800

    moveElement()

--- webnet/scripts/global.js
+++ webnet/scripts/global.js
@@ -56,3 +56,48 @@ function highlightPage() {
 	}
 }
 addLoadEvent(highlightPage);
+
+// 移动元素的函数
+// elementId：要移动的函数
+// final_x：目标X轴
+// final_y：目标Y轴
+// interval:移动的时间
+function moveElement(elementId,final_x,final_y,interval) {
+	if (!document.getElementById) return false;
+	if (!document.getElementById(elementId)) return false;
+	var elem=document.getElementById(elementId);
+	if (elem.movement) {
+		clearTimeout(elem.movement);
+	}
+	if (!elem.style.left) {
+		elem.style.left="0px";
+	}
+	if (!elem.style.top) {
+		elem.style.top="0px";
+	}
+	var xpos=parseInt(elem.style.left);
+	var ypos=parseInt(elem.style.top);
+	if (xpos==final_x && ypos ==final_y){
+		return false;
+	}
+	if (xpos < final_x) {
+		dist=Math.ceil((final_x-xpos)/10);
+		xpos=xpos+dist;
+	}
+	if (xpos > final_x) {
+		dist=Math.ceil((xpos-final_x)/10);
+		xpos=xpos-dist;
+	}
+	if (ypos < final_y) {
+		dist=Math.ceil((final_y-ypos)/10);
+		ypos=ypos+dist;
+	}
+	if (ypos > final_y) {
+		dist=Math.ceil((ypos-final_y)/10);
+		ypos=ypos-dist;
+	}
+	elem.style.left=xpos+"px";
+	elem.style.top=ypos+"px";
+	var repeat="moveElement('"+elementId+"',"+final_x+","+final_y+","+interval+")";
+	elem.movement=setTimeout(repeat,interval);
+}
