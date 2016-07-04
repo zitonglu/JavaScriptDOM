0b86de156387ff897cbe5ef947478a14776ca406
紫铜炉 <910109610@qq.com>
2016-07-04 15:55:50 +0800

    prepareInternalnav()

--- webnet/scripts/global.js
+++ webnet/scripts/global.js
@@ -146,3 +146,38 @@ function prepareSlideshow() {
 	}
 }
 addLoadEvent(prepareSlideshow);
+
+// about页面显示选中section的函数
+function showSection(id) {
+	var sections=document.getElementsByTagName('section');
+	for (var i = 0; i < sections.length; i++) {
+		if(sections[i].getAttribute('id')!=id){
+			sections[i].style.display="none";
+		}else{
+			sections[i].style.display="block";
+		}		
+	}
+}
+function preareInternalnav() {
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById) return false;
+	var articles=document.getElementsByTagName('article');
+	if (articles.length==0) return false;
+	var navs=articles[0].getElementsByTagName('nav');
+	if (navs.length==0) return false;
+	var nav=navs[0];
+	var links=nav.getElementsByTagName('a');
+	for (var i = 0; i < links.length; i++) {
+		// array=string.split(character)
+		// split是分割符作用，我们要的是ID后面的内容
+		var sectionId=links[i].getAttribute('href').split('#')[1];
+		if (!document.getElementById(sectionId)) continue;
+		document.getElementById(sectionId).style.display="none";
+		links[i].destination=sectionId;
+		links[i].onclick=function () {
+			showSection(this.destination);
+			return false;
+		}
+	}
+}
+addLoadEvent(preareInternalnav);
