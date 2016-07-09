faa4b658bef29f1f65b3243b37544fb23a35c4a7
紫铜炉 <910109610@qq.com>
2016-07-09 16:55:57 +0800

    live.html javaScript

--- webnet/scripts/global.js
+++ webnet/scripts/global.js
@@ -185,7 +185,7 @@ function preareInternalnav() {
 }
 addLoadEvent(preareInternalnav);
 
-// 显示图片函数
+// 显示图片函数 by photo.html
 // 获取图片A地址，控制不让它跳转返回
 function showPic(whichpic) {
 	if (!document.getElementById('placeholder')) return false;
@@ -236,3 +236,73 @@ function prepareGallery() {
 }
 addLoadEvent(perparePlaceholder);
 addLoadEvent(prepareGallery);
+
+// table隔行换色函数
+function stripeTables() {
+	if (!document.getElementsByTagName) return false;
+	var tables=document.getElementsByTagName('table');
+	for (var i = 0; i < tables.length; i++) {
+		var odd=false;
+		var rows=tables[i].getElementsByTagName('tr');
+		for (var j = 0; j < rows.length; j++) {
+			if (odd==true) {
+				addClass(rows[j],'odd');
+				odd=false;
+			}else{
+				odd=true;
+			}
+		}
+	}
+}
+// table高亮鼠标移动高亮函数
+function highlightRows() {
+	if (!document.getElementsByTagName) return false;
+	var rows=document.getElementsByTagName('tr');
+	for (var i = 0; i < rows.length; i++) {
+		rows[i].oldClassName=rows[i].className;
+		rows[i].onmouseover=function () {
+			addClass(this,"highlight");
+		}
+		rows[i].onmouseout=function () {
+			this.className=this.oldClassName;
+		}
+	}
+}
+// abbr函数获取
+function displayAbbreviations() {
+	if (!document.getElementsByTagName || !document.createElement || !document.createTextNode) return false;
+	var abbreviations=document.getElementsByTagName('abbr');
+	if (abbreviations.length<1) return false;
+	var defs=new Array();
+	for (var i = 0; i < abbreviations.length; i++) {
+		var current_abbr=abbreviations[i];
+		if (current_abbr.childNodes.length<1) continue;
+		var definiton=current_abbr.getAttribute('title');
+		var key=current_abbr.lastChild.nodeValue;
+		defs[key]=definiton;
+	}
+	var dlist=document.createElement('dl');
+	for (key in defs) {
+		var definiton=defs[key];
+		var dtitle=document.createElement('dt');
+		var dtitle_text=document.createTextNode(key);
+		dtitle.appendChild(dtitle_text);
+		var ddesc=document.createElement('dd');
+		var ddesc_text=document.createTextNode(definiton);
+		ddesc.appendChild(ddesc_text);
+		dlist.appendChild(dtitle);
+		dlist.appendChild(ddesc);
+	}
+	if (dlist.childNodes.length<1) return false;
+	var header=document.createElement('h3');
+	var header_text=document.createTextNode('abbreviations');
+	header.appendChild(header_text);
+	var articles=document.getElementsByTagName('article');
+	if (articles.length==0) return false;
+	var container =articles[0];
+	container.appendChild(header);
+	container.appendChild(dlist);
+}
+addLoadEvent(stripeTables);
+addLoadEvent(highlightRows);
+addLoadEvent(displayAbbreviations);
