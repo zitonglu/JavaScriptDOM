3cb807cc057ddf187f580af2850a2d0bae03b658
紫铜炉 <910109610@qq.com>
2016-07-09 16:02:38 +0800

    photos.html javaScript

--- webnet/scripts/global.js
+++ webnet/scripts/global.js
@@ -184,3 +184,55 @@ function preareInternalnav() {
 	}
 }
 addLoadEvent(preareInternalnav);
+
+// 显示图片函数
+// 获取图片A地址，控制不让它跳转返回
+function showPic(whichpic) {
+	if (!document.getElementById('placeholder')) return false;
+	var source=whichpic.getAttribute("href");
+	var placeholder=document.getElementById("placeholder");
+	placeholder.setAttribute("src",source);
+	if (!document.getElementById('description')) return false;
+	if (whichpic.getAttribute("title")) {
+		var text=whichpic.getAttribute("title");
+	}else{
+		var description=document.getElementById('description');
+		if (description.firstChild.nodeValue==3) {
+			description.firstChild.nodeValue=text;
+		}
+	}
+	return false;
+}
+// 新建站位符函数
+function perparePlaceholder() {
+	if (!document.createElement) return false;
+	if (!document.createTextNode) return false;
+	if (!document.getElementById) return false;
+	if (!document.getElementById('imagegallery'))	return false;
+	var placeholder=document.createElement("img");
+	placeholder.setAttribute('id', "placeholder");
+	placeholder.setAttribute('src', "images/placeholder.gif");
+	placeholder.setAttribute('alt', "my image gallery");
+	var description=document.createElement("p");
+	description.setAttribute('id', "description");
+	var desctext=document.createTextNode("choose an image");
+	description.appendChild(desctext);
+	var gallery=document.getElementById('imagegallery');
+	insertAfter(description,gallery);
+	insertAfter(placeholder,description);
+}
+// 重复赋予A链接函数
+function prepareGallery() {
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById) return false;
+	if (!document.getElementById('imagegallery')) return false;
+	var gallery=document.getElementById('imagegallery');
+	var links=gallery.getElementsByTagName('a');
+	for (var i = 0; i < links.length; i++) {
+		links[i].onclick=function () {
+			return showPic(this);
+		}
+	}
+}
+addLoadEvent(perparePlaceholder);
+addLoadEvent(prepareGallery);
